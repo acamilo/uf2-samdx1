edc21864f08a89f670344d7f3eeec1a21044ca4a
acamilo <alex.camilo@gmail.com>
2020-05-11 21:49:25 -0400

    Added BOARD for heater board

---
+++ boards/ref_heater_m0/board_config.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Alex Camilos Project"
+#define PRODUCT_NAME "Temp-Refence-V0"
+#define VOLUME_LABEL "TEMPREFBOOT"
+#define INDEX_URL "https://github.com/acamilo/Cheap-Feaver-Screener-PID-Control-PCB"
+#define BOARD_ID "SAMD21G18A-TempRef-v0"
+
+#define USB_VID 0x239A
+#define USB_PID 0x001B
+
+#define LED_PIN PIN_PA27
+
+#define BOARD_NEOPIXEL_PIN PIN_PA18
+#define BOARD_NEOPIXEL_COUNT 2
+
+
+#endif
